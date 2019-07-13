96b0303f79621435e5add10d1cbfd4b6997eb0b9
Jettchen <jettchen12345@gmail.com>
2019-07-13 15:31:53 +0800

    better UI+docs

--- app.py
+++ app.py
@@ -17,11 +17,15 @@ def main():
         return "此链接不合法"
     elif status == 3:
         return "没有评论"
-    elif status == 4:
-        return "开发中..."
     elif status == 0:
         # print(comments)
         return render_template('analysis.html',data=analysis(comments))
+@app.route('/help')
+def help():
+    return render_template('site/index.html')
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
